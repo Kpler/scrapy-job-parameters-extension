206debd68a5ce6d2fa225f334fdf8ccc96d94d3b
Jean Maynier <jmaynier@kpler.com>
2015-07-21 18:17:41 +0200

    fix(SCRAPY_JOB): handle case when no SCRAPY_JOB is provided.

--- scrapyjobparameters/extension.py
+++ scrapyjobparameters/extension.py
@@ -4,6 +4,15 @@ logger = logging.getLogger(__name__)
 
 class JobParametersExtension(object):
 
+    def __init__(self):
+        (project_id, spider_id, job_id) = self.retrieve_info()
+        self.project_id = project_id
+        self.spider_id = spider_id
+        self.job_id = job_id
+        logger.info('Project id : {}'.format(self.project_id))
+        logger.info('Spider id : {}'.format(self.spider_id))
+        logger.info('Job id : {}'.format(self.job_id))
+
     @classmethod
     def from_crawler(cls, crawler):
         # instantiate the extension object
@@ -16,21 +25,25 @@ class JobParametersExtension(object):
         return ext
 
     def spider_opened(self, spider):
+        spider.project_id = self.project_id
+        spider.spider_id = self.spider_id
+        spider.job_id = self.job_id
+
+    @classmethod
+    def retrieve_info(cls):
         job_name = os.environ.get('SCRAPY_JOB')
 
         #for ScrapingHub, job id is 'PROJECT_ID/SPIDER_ID/JOB_ID'
         try:
             (project_id, spider_id, job_id) = job_name.split('/')
-        except ValueError:
-            # if not enpught value to unpack this is not a SH id, so keep the all string
+        except (ValueError, AttributeError):
+            # if not enough value to unpack this is not a SH id, so keep the all string
             job_id = job_name
             spider_id = None
             project_id = os.environ.get('SCRAPY_PROJECT_ID')
 
-        spider.project_id = project_id
-        spider.spider_id = spider_id
-        spider.job_id = job_id
+        os.environ['SCRAPY_PROJECT_ID'] = project_id
+        os.environ['SCRAPY_JOB_ID'] = job_id
+        os.environ['SCRAPY_SPIDER_ID'] = spider_id
 
-        logger.info('Project id : {}'.format(spider.project_id))
-        logger.info('Spider id : {}'.format(spider.spider_id))
-        logger.info('Job id : {}'.format(spider.job_id))
+        return project_id, spider_id, job_id
