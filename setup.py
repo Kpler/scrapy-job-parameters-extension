331725f44992a4648b403e19a6262d4ab89222a6
cmorent <cmorent@dailymotion.com>
2016-12-28 11:25:33 +0100

    Fix packaging issue

--- setup.py
+++ setup.py
@@ -18,13 +18,13 @@ setup(
     name='scrapyjobparameter',
     version=scrapyjobparameters.__version__,
     description='Scrapy extension to make job_id and project_id available as spider fields.',
-    long_description=open('README.md').read(),
+    #long_description=open('README.md').read(),
     author='Jean Maynier',
     author_email='jmaynier@kpler.com',
     url='http://github.com/kpler/scrapy-job-parameters-extension',
     packages=packages,
     install_requires=requires,
-    license=open('LICENSE').read(),
+    #license=open('LICENSE').read(),
     classifiers=(
         'Development Status :: 4 - Beta'
         'Intended Audience :: Developers',
