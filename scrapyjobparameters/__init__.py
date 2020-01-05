e21735f27936619e81d50f4d554797aa4191e00e
Sébastien Douche <seb@nmeos.net>
2020-01-05 16:44:44 +0100

    Clean setup.py

--- scrapyjobparameters/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__package__ = 'scrapyjobparameters'
-__version__ = '0.1.9'
--- setup.py
+++ setup.py
@@ -1,48 +1,45 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# as inspired by https://github.com/kennethreitz/setup.py
-
-from __future__ import absolute_import, unicode_literals
-import codecs
 import os
-from setuptools import setup
+import codecs
+from setuptools import find_packages, setup
 
-from scrapyjobparameters import __package__, __version__
-
-
-packages = [
-    __package__,
-]
-
-REQUIRED = [
-    'scrapy',
-]
 
 here = os.path.abspath(os.path.dirname(__file__))
-
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.rst' is present in your MANFEST.in file!
 with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = '\n' + f.read()
 
+
 setup(
-    name=__package__,
-    version=__version__,
-    description='Scrapy extension to make job_id and project_id available as spider fields.',
+    name='scrapy-job-parameters',
+    description='Scrapy downloader middleware to enable persistent storage.',
     long_description=long_description,
-    author='Jean Maynier',
-    author_email='jmaynier@kpler.com',
+    author='Kpler engineering',
+    author_email='engineering@kpler.com',
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    packages=find_packages(),
     url='http://github.com/kpler/scrapy-job-parameters-extension',
-    packages=packages,
-    install_requires=REQUIRED,
-    include_package_data=True,
-    classifiers=(
-        'Development Status :: 4 - Beta'
+    classifiers=[
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-    ),
+    ],
+    install_requires = [
+        'scrapy',
+    ],
+    extras_require={
+        "test": [
+            'coverage==4.5',
+            'flake8==3.5.0',
+            'ipdb==0.10.3',
+            'mock==2.0.0',
+            'nose==1.3.7',
+            'nose-cov==1.6',
+            'nose-timer==0.7.1',
+            'tox==3.0.0',
+            'tox-pyenv==1.1.0'
+        ]
+    }
 )
